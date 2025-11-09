464b3c6ce2d12f78bd4d680fd4f14521f384f229
manthymanthy <121672327+manthymanthy@users.noreply.github.com>
2025-11-09 21:33:41 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import GameFilter from "../components/GameFilter";
 
 export const revalidate = 0;
 export const dynamic = "force-dynamic";
-export const fetchCache = "force-no-store";.
+export const fetchCache = "force-no-store";
 
 type Row = {
   id: string;
